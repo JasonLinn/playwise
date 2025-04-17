26dcf0d91e31dbba1459feb05ddd9a4e2b7f46bf
Your Name <your-email@example.com>
2025-04-17 20:35:32 +0800

    添加Google站點驗證meta標籤

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "智遊工作室 | 讓工作更輕鬆，讓生活更自由",
   description: "我們幫助小型品牌、個人工作者、民宿、店家，把繁瑣的日常工作變得更聰明、更簡單。提供自動化流程、智能客服小幫手和工作模式規劃服務。",
+  verification: {
+    google: "Q3eim92P3kSmJF6ea3Ebb04rnrGa5Us36AtWsGvCrwk",
+  },
 };
 
 export default function RootLayout({
